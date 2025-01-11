2ad08f560a04cbc77316f7dece5399e445281879
rodrigo <rocasgut@gmail.com>
2025-01-11 17:11:37 +0100

    Mejora de programa de BooleanConIf

--- src/main/java/com/mycompany/app/BooleanConIf.java
+++ src/main/java/com/mycompany/app/BooleanConIf.java
@@ -1,37 +1,35 @@
 package com.mycompany.app;
-
-import java.util.Scanner;
-
+import java.util.*;
 public class BooleanConIf {
-    public static void main(String[] args) {
-
-        Scanner entrada = new Scanner(System.in);
-        boolean oferta=false; //segun cambie el valor de ofertar entre true/false aparecera la compra de auriculares o no 
-        System.out.println("¿Que nota tienes?:");
-        int nota=entrada.nextInt();
-
-        System.out.println("¿Estamos de oferta?:");
-        oferta = entrada.nextBoolean(); //segun contestemos con un true/false ejecuta el if de oferta o no 
+    public static void main (String args[]){
+        Scanner entrada = new Scanner (System.in);
+        boolean oferta = false;
+       
+        System.out.println("¿Que nota tienes? :");
+        int nota = entrada.nextInt();
         
-        if (nota<5 && nota>3) {
-            System.out.println("¡¡Hay que estudiar mas !!");
-        } else if(nota>=0 && nota<=3){
+        if(nota>=6 && nota<=10){
+            System.out.println("¿Estan los articulon de oferta?");
+            oferta = entrada.nextBoolean();
+            if (nota>5 && nota<=8){
+                System.out.println("¡¡Compramos el balón!!");
+                if (oferta==true){
+                    System.out.println("¡¡Con la camiseta de cristiano!!");
+                }
+            } else if (nota<=10 && nota>=9){
+                System.out.println("¡¡Compramos el movil!!");
+                if(oferta==true){
+                    System.out.println("¡¡Con auriculares!!");
+                }
+            }else if (nota<0 || nota>10){
+                System.out.println("valor invalido");
+            }
+        }else if ( nota>=3 && nota<5 ){
+            System.out.println("Hay que estudiar mas.");
+        } else if (nota<3  && nota>=0){
             System.out.println("Muy mal");
-        }else if (nota==5) {
+        }else if (nota==5){
             System.out.println("Has aprobado muy justito");
-        }else if(nota>5 && nota<9) {
-            System.out.println("Compramos el balon!");
-            if (oferta==true){
-                System.out.println("¡¡Con camiseta de RONALDO!!");
-            }
-        }else if(nota==10 && nota>=9){
-            System.out.println("Compramos el movil!");
-            if (oferta==true){
-                System.out.println("¡¡Con auriculares!!");
-            }
-        }if (nota<0 || nota>10){
-            System.out.println("Datos Invalidos");
         }
     }
-
 }
