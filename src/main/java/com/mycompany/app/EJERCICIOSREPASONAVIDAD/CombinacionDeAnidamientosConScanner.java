c8f966ca3e9d41555b84094a78954fc0e02057a6
rodrigo <rocasgut@gmail.com>
2025-01-14 17:17:47 +0100

    anidamientos con scanner

---
+++ src/main/java/com/mycompany/app/EJERCICIOSREPASONAVIDAD/CombinacionDeAnidamientosConScanner.java
@@ -0,0 +1,21 @@
+package com.mycompany.app.EJERCICIOSREPASONAVIDAD;
+
+import java.util.Scanner;
+
+public class CombinacionDeAnidamientosConScanner {
+    public static void main(String[] args) {
+        System.out.println("BIENVENIDO");
+        Scanner entrada = new Scanner (System.in);
+        int i; 
+        System.out.println("¿Por que numero quieres dividir?: ");
+        int numero = entrada.nextInt();
+        int iteraciones = 0;
+        for (i=1 ; i<=100; i++){
+            if (i%numero == 0){
+                iteraciones ++;
+            }
+        }
+        System.out.println("---------------------------------------------------");
+        System.out.println("El numero 100 es divisible entre " + numero + " este numero de veces: " + iteraciones);   
+    }
+}
